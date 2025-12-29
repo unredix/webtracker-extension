a703a2ba3e2f46262d98017237d13e1a0decba88
Unredix <999daved@gmail.com>
2025-12-29 23:08:32 +0100

    Completed options page

--- options/options.js
+++ options/options.js
@@ -1,1 +1,113 @@
-// TODO: Start this
+document.addEventListener("DOMContentLoaded", function () {
+  const ignoreListInput = document.getElementById("ignore-url");
+  const ignoreList = document.getElementById("ignored-sites-list");
+  const saveButton = document.getElementById("save-changes");
+  const cancelButton = document.getElementById("cancel-changes");
+  const ignoreIncognitoCheckbox = document.getElementById("ignore-incog");
+  const languageSelect = document.getElementById("lang");
+
+  function getStoredOptions() {
+    chrome.storage.sync.get(
+      ["ignoredSites", "ignoreInIncognito", "language"],
+      function (data) {
+        const ignoredSites = data.ignoredSites || [];
+        ignoredSites.forEach(function (site) {
+          const listItem = document.createElement("li");
+          const itemDeleteButton = document.createElement("button");
+
+          itemDeleteButton.textContent = "✖";
+          itemDeleteButton.style.cursor = "pointer";
+          itemDeleteButton.style.border = "none";
+          itemDeleteButton.style.borderRadius = "3px";
+          itemDeleteButton.style.backgroundColor = "rgb(133, 133, 255)";
+          itemDeleteButton.style.marginRight = "10px";
+          itemDeleteButton.addEventListener("click", function () {
+            ignoreList.removeChild(listItem);
+            showButtons();
+          });
+
+          listItem.appendChild(itemDeleteButton);
+          const siteText = document.createTextNode(site);
+          listItem.appendChild(siteText);
+
+          ignoreList.appendChild(listItem);
+        });
+
+        ignoreIncognitoCheckbox.checked = data.ignoreInIncognito || false;
+
+        if (data.language) {
+          languageSelect.value = data.language;
+        }
+      }
+    );
+  }
+
+  function showButtons() {
+    saveButton.style.display = "block";
+    cancelButton.style.display = "block";
+  }
+
+  function hideButtons() {
+    saveButton.style.display = "none";
+    cancelButton.style.display = "none";
+  }
+
+  getStoredOptions();
+
+  document.getElementById("lang").addEventListener("change", function () {
+    showButtons();
+  });
+
+  document
+    .getElementById("ignore-incog")
+    .addEventListener("change", function () {
+      showButtons();
+    });
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      const site = ignoreListInput.value.trim().toLowerCase();
+      if (site) {
+        const listItem = document.createElement("li");
+        listItem.textContent = site;
+        ignoreList.appendChild(listItem);
+        ignoreListInput.value = "";
+        showButtons();
+      }
+    }
+  });
+
+  cancelButton.addEventListener("click", function () {
+    ignoreList.innerHTML = "";
+
+    getStoredOptions();
+    hideButtons();
+  });
+
+  saveButton.addEventListener("click", function () {
+    const ignoredSites = [];
+    Array.from(ignoreList.children).forEach(function (item) {
+      if (item.textContent && !ignoredSites.includes(item.textContent)) {
+        ignoredSites.push(
+          item.textContent[0] === "✖"
+            ? item.textContent.slice(1).trim()
+            : item.textContent.trim()
+        );
+      }
+    });
+
+    const ignoreInIncognito = ignoreIncognitoCheckbox.checked;
+    const language = languageSelect.value;
+
+    chrome.storage.sync.set({
+      ignoredSites: ignoredSites,
+      ignoreInIncognito: ignoreInIncognito,
+      language: language,
+    });
+
+    ignoreList.innerHTML = "";
+    getStoredOptions();
+
+    hideButtons();
+  });
+});
--- popup/popup.js
+++ popup/popup.js
@@ -9,4 +9,18 @@ document.addEventListener("DOMContentLoaded", async function () {
   // This is the clean name without the unnecessary parts
   const hostname = new URL(tab.url).hostname;
   document.getElementById("cSite").textContent = hostname.toUpperCase();
+
+  const syncStorage = await chrome.storage.sync.get([
+    "ignoredSites",
+    "ignoreInIncognito",
+  ]);
+
+  if (
+    syncStorage.ignoredSites.includes(hostname) === true ||
+    (tab.incognito && syncStorage.ignoreInIncognito === true)
+  ) {
+    document.getElementById("status").textContent =
+      "Tracking blocked on this site.";
+    document.getElementById("status").style.display = "block";
+  }
 });
