c0e4f630079a1a8629a32f3584fa44fc6a552a17
Unredix <999daved@gmail.com>
2025-12-30 19:16:19 +0100

    Background.js started

--- background.js
+++ background.js
@@ -1,2 +1,13 @@
-let currentTab = null;
-let tabs = {};
+const ignoredSites = null;
+
+async function initializeExtension() {
+  try {
+    ignoredSites = await chrome.storage.sync.get("ignoredSites");
+  } catch (error) {}
+}
+
+chrome.runtime.onStartup.addListener(() => {
+  console.log("Extension has started up");
+
+  initializeExtension();
+});
